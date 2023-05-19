a248739152774180d4c40f284798d55b7e6b2904
Ian Jonas Hiroce <hirocex@gmail.com>
2023-05-19 15:28:00 -0300

    third video commit

--- introducao-frontend-javascript-assincrono/gerador-de-senhas/src/main.js
+++ introducao-frontend-javascript-assincrono/gerador-de-senhas/src/main.js
@@ -1,2 +1,8 @@
 import { nanoid } from "nanoid";
 
+const randomPassword = nanoid()
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+passwordBtnEl.addEventListener('click', () => {
+    displayPasswordEl.innerHTML = randomPassword;
+})
