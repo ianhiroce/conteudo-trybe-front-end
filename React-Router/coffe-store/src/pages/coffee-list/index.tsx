868f2050da075ff9e4200abab5238550ef44f325
Ian Jonas Hiroce <hirocex@gmail.com>
2023-06-12 14:38:25 -0300

    fix

---
+++ React-Router/coffe-store/src/pages/coffee-list/index.tsx
@@ -0,0 +1,5 @@
+export default function CoffeList() {
+  return (
+    <h1>CoffeeList page</h1>
+  );
+}
---
+++ React-Router/coffe-store/src/pages/coffee/index.tsx
@@ -0,0 +1,5 @@
+export default function Coffee() {
+  return (
+    <h1>Coffee page</h1>
+  );
+}
