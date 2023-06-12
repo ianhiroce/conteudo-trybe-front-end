797fa5c81cc86e84ebf3985acfe23e4cd70485d0
Ian Jonas Hiroce <hirocex@gmail.com>
2023-06-12 14:33:20 -0300

    fix

--- React-Router/coffe-store/src/App.tsx
+++ React-Router/coffe-store/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    //     <BrowserRouter>
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {/*     </BrowserRouter> */}
     </>
   )
 }
